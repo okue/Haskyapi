6eb3c476bfbac37a508397624026a4bd8883cc3b
okue <nogideca@gmail.com>
2017-10-10 21:20:30 +0900

    push

--- src/Api/Hapi.hs
+++ src/Api/Hapi.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE ForeignFunctionInterface #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE MultiWayIf #-}
 module Api.Hapi (
   routing,
 ) where
@@ -8,6 +10,12 @@ import Network.HTTP.Simple
 import Text.HTML.TagSoup (parseTags, Tag(..))
 import qualified Codec.Binary.UTF8.String as B8
 import qualified Data.ByteString.Lazy.Char8 as LC
+import Control.Applicative
+import Data.Aeson
+import Data.Maybe (catMaybes)
+import GHC.Generics
+
+import Debug.Trace (trace)
 
 import qualified Web.Haskyapi.Tool as Tool
 import Web.Haskyapi.Header (
@@ -23,18 +31,53 @@ import Web.Haskyapi.Header (
 -- foreign import ccall "two"  c_two  :: CInt
 -- foreign import ccall "moji" c_moji :: CInt -> CString
 
-
 routing :: [Api]
 routing = [
-            (GET,  "/test",  test GET),
-            (POST, "/test",  test POST),
-            (GET,  "/test2", test2),
-            (POST, "/test2", test2),
-            (GET,  "/add",   add),
-            (POST, "/add",   add),
-            (GET,  "/title", title),
-            (POST, "/title", title)
+             (GET,  "/test",  test GET)
+            ,(POST, "/test",  test POST)
+            ,(GET,  "/test2", test2)
+            ,(POST, "/test2", test2)
+            ,(GET,  "/add",   add)
+            ,(POST, "/add",   add)
+            ,(GET,  "/title", title)
+            ,(POST, "/title", title)
+            ,(POST, "/checkout", checkout)
+            ,(GET,  "/checkout", checkout)
           ]
+
+-- head
+--   Content-Type: application/json
+-- body
+--   { json file }
+--
+-- http://localhost:8080/api/checkout
+--
+newtype SRequest = SRequest { order :: [String] } deriving (Show, Generic)
+data SResponse  = SResponse  { ok :: Bool, amount :: Int, items :: [String] }
+                | SResponse2 { ok :: Bool, message :: String }
+                deriving (Show)
+instance FromJSON SRequest
+instance ToJSON SResponse where
+  toJSON (SResponse  x y z) = object [ "ok" .= x, "amount" .= y, "items" .= z ]
+  toJSON (SResponse2 x y)   = object [ "ok" .= x, "message" .= y ]
+
+hamb  = [("101",100),("102",130),("103",320),("104",320),("105",380)]
+side  = [("201",150),("202",270),("203",320),("204",280)]
+drink = [("301",100),("302",220),("303",250),("304",150),("305",240),("306",270),("307",100),("308",150)]
+
+checkout :: ApiFunc
+checkout qry = do
+  -- let xx   = "{\"order\": [ \"101\", \"201\", \"301\", \"444\" ]}"
+  let xx   = "{\"order\": [ \"101\", \"201\", \"301\" ]}"
+      menu = hamb ++ side ++ drink
+  case (decode xx :: Maybe SRequest) of
+    Nothing  -> return "bad"
+    Just srq -> do
+      let yy = map (`lookup` menu) (order srq)
+      if | Nothing `elem` yy -> return . LC.unpack . encode $ SResponse2 False "item_not_found"
+         | otherwise -> do
+             let ss = SResponse True (sum (catMaybes yy)) (order srq)
+             return . LC.unpack $ encode ss
 
 
 title :: ApiFunc
@@ -72,3 +115,6 @@ add qry =
     (_,Nothing) -> "No value of y."
     (Just x, Just y) -> show $ read x + read y
 
+main = do
+  x <- checkout []
+  putStrLn x
