20c5bd300f5108d1bc6517fb79ba8c40c7efb902
okue <nogideca@gmail.com>
2017-10-12 01:32:57 +0900

    save

--- app/Model.hs
+++ app/Model.hs
@@ -1,44 +1,85 @@
-{-# LANGUAGE GADTs                      #-}
-{-# LANGUAGE QuasiQuotes                #-}
-{-# LANGUAGE TypeFamilies               #-}
-{-# LANGUAGE EmptyDataDecls             #-}
-{-# LANGUAGE TemplateHaskell            #-}
-{-# LANGUAGE FlexibleContexts           #-}
-{-# LANGUAGE OverloadedStrings          #-}
-{-# LANGUAGE MultiParamTypeClasses      #-}
+{-# LANGUAGE QuasiQuotes, TemplateHaskell, TypeFamilies #-}
+{-# LANGUAGE OverloadedStrings, GADTs, FlexibleContexts #-}
+{-# LANGUAGE EmptyDataDecls, MultiParamTypeClasses      #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
-module Model
-where
+module Model (
+  lookupMenu,
+  lookupCoupon
+) where
 
+import Data.Maybe (listToMaybe)
+import Control.Monad.Reader   (ReaderT)
 import Control.Monad.IO.Class (liftIO)
 import Database.Persist
 import Database.Persist.TH
 import Database.Persist.Sqlite
 
 share [mkPersist sqlSettings, mkMigrate "migrateAll"] [persistLowerCase|
-  Person
-    name String
-    age Int Maybe
+  Menu
+    key   Int
+    price Int
     deriving Show
-  BlogPost
-    title String
-    authorId PersonId
+  Coupon
+    key String
+    off Int
+    deriving Show
+  SetMenu
+    key Int
+    menu [Int]
     deriving Show
 |]
 
-main :: IO ()
-main = runSqlite "./haskyapi.db" $ do
+lookupTemplate f g =
+  runSqlite "./haskyapi.db" . aux $ listToMaybe . map (f . entityVal) <$> g
+  where
+    aux :: ReaderT SqlBackend m a -> ReaderT SqlBackend m a
+    aux = id
+
+lookupCoupon :: String -> IO (Maybe Int)
+lookupCoupon x =
+  lookupTemplate couponOff $ selectList [CouponKey ==. x] [LimitTo 1]
+
+lookupMenu :: Int -> IO (Maybe Int)
+lookupMenu x =
+  lookupTemplate  menuPrice $ selectList [MenuKey ==. x] [LimitTo 1]
+
+insertMenu :: IO ()
+insertMenu = runSqlite "./haskyapi.db" $ do
   runMigration migrateAll
+  mapM_ insert [
+      Menu 101 100,
+      Menu 102 130,
+      Menu 103 320,
+      Menu 104 320,
+      Menu 105 380,
+      Menu 201 150,
+      Menu 202 270,
+      Menu 203 320,
+      Menu 204 280,
+      Menu 301 100,
+      Menu 302 220,
+      Menu 303 250,
+      Menu 304 150,
+      Menu 305 240,
+      Menu 306 270,
+      Menu 307 100,
+      Menu 308 150
+    ]
+  mapM_ insert [
+      Coupon "C001" 120,
+      Coupon "C002" 170,
+      Coupon "C003" 50,
+      Coupon "C004" 70,
+      Coupon "C005" 80,
+      Coupon "C006" 50
+    ]
 
-  johnId <- insert $ Person "John Doe" $ Just 35
-  janeId <- insert $ Person "Jane Doe" Nothing
-
-  insert $ BlogPost "My fr1st p0st" johnId
-  insert $ BlogPost "One more for good measure" johnId
-
-  oneJohnPost <- selectList [BlogPostAuthorId ==. johnId] [LimitTo 1]
-  liftIO $ print (oneJohnPost :: [Entity BlogPost])
-  john <- get johnId
-  liftIO $ print (john :: Maybe Person)
-  delete janeId
-  deleteWhere [BlogPostAuthorId ==. johnId]
+main :: IO ()
+main = do
+  insertMenu
+  print =<< lookupMenu 101
+  print =<< lookupMenu 201
+  print =<< lookupMenu 301
+  print =<< lookupMenu 901
+  print =<< lookupCoupon "C001"
+  print =<< lookupCoupon "C004"
