bed167d8abe6d34b8df68d501cd32fb7eef862c3
okue <nogideca@gmail.com>
2017-10-20 11:38:01 +0900

    update

--- app/Api/httpget.hs
+++ app/Api/httpget.hs
@@ -1,17 +1,13 @@
+#! /usr/bin/env stack runhaskell
 {-# LANGUAGE OverloadedStrings #-}
 import Network.HTTP.Simple
 import qualified Data.ByteString.Lazy.Char8 as L8
 
-sample l = "{ \"order\" : " ++ l ++ " }"
-
-s1 = sample . show $ map show [103, 202, 302]
-s2 = sample . show $ map show [107, 202, 302]
-
 main :: IO ()
 main = do
-  let req' = setRequestMethod "POST" "http://localhost:8080/api/checkout"
-      req  = setRequestBodyLBS (L8.pack s1) req'
+  let req' = setRequestMethod "POST" "http://example.com"
+      req  = setRequestBodyLBS (L8.pack "1") .
+             setRequestHeaders [("Content-Type","application/json")] $ req'
   res <- httpLBS req
-  print $ getResponseHeader "Content-Type" res
   L8.putStrLn $ getResponseBody res
 
